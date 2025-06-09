b9f934778a6dc6d566eab0dea0852c8e5670c4ac
SehashDananjaya <sehashdananjaya@gmail.com>
2025-06-09 23:22:07 +0530

    product

--- src/main/java/service/ServiceFactory.java
+++ src/main/java/service/ServiceFactory.java
@@ -1,4 +1,25 @@
 package service;
 
+import service.custom.impl.ProductServiceImpl;
+import util.ServiceType;
+
 public class ServiceFactory {
+    private static ServiceFactory instance;
+
+    private ServiceFactory(){}
+
+    public static ServiceFactory getInstance() {
+        return instance==null?instance=new ServiceFactory():instance;
+    }
+
+    public <T extends SuperService>T getServiceType(ServiceType type){
+
+        switch (type){
+            case PRODUCT: return (T) new ProductServiceImpl();
+           // case ITEM: return (T) new ItemServiceImpl();
+        }
+        return null;
+
+    }
 }
+g
