1edfa6cc2050649ee147d0f5867ae70e4d8847cd
SehashDananjaya <sehashdananjaya@gmail.com>
2025-07-02 23:45:53 +0530

    login page

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistrationOnAction(ActionEvent event) {
+
+    }
+
+}
